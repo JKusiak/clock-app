cd6ee5c5755e13cb982580033e7c20d154130d85
224094 Lemiesz Aleksander <224094@edu.p.lodz.pl>
2021-01-07 00:55:32 +0100

    App does not crash and can set an hour in editText.

--- AlarmClockApp/app/src/main/java/com/example/alarmclockapp/MainActivity.java
+++ AlarmClockApp/app/src/main/java/com/example/alarmclockapp/MainActivity.java
@@ -1,89 +1,92 @@
 package com.example.alarmclockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText hourET;
+    private TextView helloTV;
+    Character helloChar = ' ';
+    String helloString = " ";
 
-    BluetoothAdapter myBluetooth;
-    BluetoothSocket btSocket;
+    // Variables for connection
+    String address = null;
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
     private boolean isBtConnected = false;
-    String address;
+    //SPP UUID. Look for it
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    char mainText = ' ';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            if (btSocket == null || !isBtConnected) {
-                myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
-                BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
-                btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
-                BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-                btSocket.connect();//start connection
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        hourET = findViewById(R.id.hourET);
+        hourET.setFocusable(false);
+
+        helloTV = findViewById(R.id.tvMain);
+        helloString = Character.toString(helloChar);
+        helloTV.setText(helloString);
+
+    }
+
+    public void showTimePicker(View v) {
+        DialogFragment newFragment = new TimePickerFragment(hourET);
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+        private EditText text;
+
+        public TimePickerFragment(EditText v) {
+            this.text = v;
         }
 
-        TextView output = findViewById(R.id.tvMain);
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
 
-        try {
-            mainText = (char) btSocket.getInputStream().read();
-        } catch (IOException e) {
-            e.printStackTrace();
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
         }
-        output.setText(mainText);
 
-        Button sendBtn = findViewById(R.id.sendBtn);
-        sendBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (btSocket!=null)
-                {
-                    try
-                    {
-                        btSocket.getOutputStream().write('1');
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+
+            String time;
+            if (hourOfDay < 10 & minute < 10) {
+                time = "0" + hourOfDay + ":0" + minute;
+            } else if (hourOfDay > 10 & minute < 10) {
+                time = "" + hourOfDay + ":0" + minute;
+            } else if (hourOfDay < 10 & minute > 10) {
+                time = "0" + hourOfDay + ":" + minute;
+            } else {
+                time = "" + hourOfDay + ":" + minute;
             }
-        });
-
-        Button receiveBtn = findViewById(R.id.recieveBtn);
-        receiveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (btSocket!=null)
-                {
-                    try
-                    {
-                        mainText = (char) btSocket.getInputStream().read();
-                        output.setText(mainText);
-                    }
-                    catch (IOException e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
+            text.setText(time);
+        }
     }
 }
